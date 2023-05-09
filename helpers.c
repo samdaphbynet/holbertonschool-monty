ebf9226aac4978ebef10c74c0460634ba4666d46
samdaphbynet <lj8@hotmail.fr>
2023-05-09 18:42:19 +0200

    update function

--- helpers.c
+++ helpers.c
@@ -1,6 +1,42 @@
 #include "monty.h"
 
 /**
+ * opcode - Get the function associated to an opcode and execute it
+ *
+ * @top: Top of the stack
+ * @arg: Current argument in the bytecode file
+ * @line: Current line in the stack
+ *
+ * Return: (void)
+ */
+
+void opcode(stack_t **top, char *arg, unsigned int line)
+{
+	int i;
+
+	instruction_t op[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{NULL, NULL}
+	};
+
+	for (i = 0; op[i].opcode; i++)
+	{
+		if (strcmp(op[i].opcode, arg) == 0)
+		{
+			op[i].f(top, line);
+			return;
+		}
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", line, arg);
+	free_stack(*top);
+	exit(EXIT_FAILURE);
+}
+
+/**
  * free_stack - Free a stack structure
  *
  * @top: Top of the stack
