c82b7fd09925a5c42c5b4d6c6febcc265580e4f1
samdaphbynet <lj8@hotmail.fr>
2023-05-09 18:26:32 +0200

    update main function

--- main.c
+++ main.c
@@ -1,34 +1,49 @@
 #include "monty.h"
+#include <fcntl.h>
 
-int main(void)
+/**
+ * main - Monty bytecodes files interpreter
+ *
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: 0 on SUCCESS
+ */
+
+int main(int ac, char **av)
 {
-    stack_t *stack = NULL;
-    char opcode[10];
-    unsigned int line_number = 1;
-    int value;
+	FILE *fd;
+	char *buffer = NULL, *op = NULL;
+	stack_t *top = NULL;
+	size_t byteCount;
+	unsigned int line = 1;
 
-    while (scanf("%s", opcode) != EOF)
-    {
-        if (strcmp(opcode, "push") == 0)
-        {
-            if (scanf("%d", &value) != 1)
-            {
-                fprintf(stderr,"L%d: usage: push interger\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            push(&stack, line_number);
-            stack->n = value;
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall(&stack, line_number);
-        }
-        else
-        {
-            printf("L%d: unknown opcode %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-        line_number++;
-    }
-    return (0);
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(av[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&buffer, &byteCount, fd) != -1)
+	{
+		op = strtok(buffer, " $\n");
+		global.arg = strtok(NULL, " $\n");
+
+		if (op)
+			opcode(&top, op, line);
+
+		line++;
+	}
+
+	free(buffer);
+	free_stack(top);
+	fclose(fd);
+	return (0);
 }
