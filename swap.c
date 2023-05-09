22264a826dc30fe136ca7052ff8b9d73ccdee25f
samdaphbynet <lj8@hotmail.fr>
2023-05-09 23:46:19 +0200

    fixed betty style

--- swap.c
+++ swap.c
@@ -1,26 +1,32 @@
 #include "monty.h"
 
+/**
+ * swap - function that swaps the top two elements of the stack.
+ * @top: the head of structur
+ * @i: number of line
+ */
+
 void swap(stack_t **top, unsigned int i)
 {
-    stack_t *tmp;
-    int length = 0;
-    int input;
+	stack_t *tmp;
+	int length = 0;
+	int input;
 
-    tmp = *top;
-    while (tmp)
-    {
-        tmp = tmp->next;
-        length++;
-    }
+	tmp = *top;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		length++;
+	}
 
-    if (length < 2)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", i);
-        free(*top);
-        exit(EXIT_FAILURE);
-    }
-    tmp = *top;
-    input = tmp->n;
-    tmp->n = tmp->next->n;
-    tmp->next->n = input;
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", i);
+		free(*top);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *top;
+	input = tmp->n;
+	tmp->n = tmp->next->n;
+	tmp->next->n = input;
 }
