ed278350e12e73ee9cac523c08237dfb0cd1ad85
samdaphbynet <lj8@hotmail.fr>
2023-05-10 15:56:41 +0200

    The purpose of this function is nothing

---
+++ nop.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * nop - The purpose of this function is nothing, literally
+ *
+ * @top: Doesn't matter
+ * @line: Who cares
+ *
+ * Return: Nothing, in case you hadn't noticed
+ */
+
+void nop(stack_t **top, unsigned int line)
+{
+	(void) top;
+	(void) line;
+}
+
