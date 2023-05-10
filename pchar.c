40bfe5fa48553a58f822ce5cc27819fcd1a2a7ba
samdaphbynet <lj8@hotmail.fr>
2023-05-10 16:27:12 +0200

    prints the char at the top of the stack

--- pchar.c
+++ pchar.c
@@ -8,21 +8,17 @@
 
 void pchar(stack_t **top, unsigned int line)
 {
-	stack_t *tmp;
-
-	tmp = *top;
-
-	if (!tmp)
+	if (!*top)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
 		free(*top);
 		exit(EXIT_FAILURE);
 	}
-	if (tmp->n > 127 || tmp->n < 0)
+	if ((*top)->n > 127 || (*top)->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line);
 		free(*top);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", tmp->n);
+	printf("%c\n", (*top)->n);
 }
