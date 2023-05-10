26d03119d45a5fd3aaf932a04c41f5846aee344e
samdaphbynet <lj8@hotmail.fr>
2023-05-10 18:56:02 +0200

    delete free top

--- pchar.c
+++ pchar.c
@@ -11,14 +11,12 @@ void pchar(stack_t **top, unsigned int line)
 	if (!*top)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
-		free(*top);
 		exit(EXIT_FAILURE);
 	}
 	if ((*top)->n > 127 || (*top)->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line);
-		free(*top);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", (*top)->n);
+	printf("%c\n",(char)(*top)->n);
 }
